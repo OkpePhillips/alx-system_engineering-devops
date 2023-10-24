be8f3292f7797c3ca64e7ab250312990ab4c2b41
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-10-24 02:32:57 -0700

    Fixed module name error and some issues

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
 """
-Module to access an api endpoint and print the output.
+Module to access an api endpoint using requests
+and print the data retrieved in nicely formatted way
 """
+import requests
+import sys
+
 if __name__ == '__main__':
-    import requests
-    import sys
-
-
-    def access_api(employee_id):
-        """
-        Method to fetch todo list information of an
-        employee.
-        """
-        user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        employee = requests.get(user_url).json()
-        employee_name = employee.get("name")
-        todo = requests.get(todo_url).json()
-        completed = 0
-        total = 0
-        completed_task = []
-        for id in todo:
-            if id['completed']:
-                completed += 1
-                completed_task.append(id['title'])
-            total += 1
-        print("Employee {} is done with tasks({}/{})".format(
-              employee_name, completed, total))
-        for task in completed_task:
-            print('\t {}'.format(task))
-
-access_api(sys.argv[1])
+    employee_id = sys.argv[1]
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    todo_url = f'https://jsonplaceholder.typicode.com/todos?'\
+               f'userId={employee_id}'
+    employee = requests.get(user_url).json()
+    employee_name = employee.get("name")
+    todo = requests.get(todo_url).json()
+    completed = 0
+    total = 0
+    completed_task = []
+    for id in todo:
+        if id['completed']:
+            completed += 1
+            completed_task.append(id['title'])
+        total += 1
+    print("Employee {} is done with tasks({}/{})".format(
+          employee_name, completed, total))
+    for task in completed_task:
+        print('\t {}'.format(task))
