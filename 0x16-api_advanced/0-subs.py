6a736c34cf951c17d27e1c5dba5cb334213cd6d5
Godwin Okpe <okpegodwin18@yahoo.com>
2023-11-08 04:54:08 -0800

    Fetching subscribers list from a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,4 +3,17 @@
 A script that returns the number of subscribers of a subreddit
 using the reddit api.
 """
+import requests
+import sys
 
+
+def number_of_subscribers(subreddit):
+    """ Queries to Reddit API """
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-Agent": "AdvancedApi"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    return data['data']['subscribers']
