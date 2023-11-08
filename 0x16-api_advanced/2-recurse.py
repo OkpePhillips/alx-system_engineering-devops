28fba8117f5bedf0cf2491bce75212a75b64c49b
Godwin Okpe <okpegodwin18@yahoo.com>
2023-11-08 12:49:23 -0800

    Fixing errors

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -16,7 +16,8 @@ def recurse(subreddit, hot_list=[], after=None):
 
     headers = {'User-Agent': 'api_advanced'}
     response = requests.get(url, params=params,
-                            headers=headers, allow_redirects=False)
+                            headers=headers,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
